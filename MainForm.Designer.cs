481ad6b00f6dff5f352707ed899fcc2ed65820b3
customsmtr <customsimtr@gmail.com>
2024-12-08 13:28:51 +0300

    Create MainForm.Designer.cs

---
+++ MainForm.Designer.cs
@@ -0,0 +1,25 @@
+namespace HidSharpTest
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Name = "MainForm";
+            this.Text = "32 Button Gamepad";
+        }
+    }
+}
